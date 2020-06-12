4117078e01c1aec29683becd7410fc1255d12714
AfifurRochman <61938343+AfifurRochman@users.noreply.github.com>
2020-06-12 19:23:45 +0700

    Create siomay.cs

---
+++ siomay.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstraction
+{
+    public class siomay : Elektronik
+    {
+        public override void smartphone()
+        {
+            Console.WriteLine("ini adalah barang elektronik jenis smartphone yang memiliki versi");
+            Console.WriteLine("note, note+ , ");
+        }
+    }
+}
