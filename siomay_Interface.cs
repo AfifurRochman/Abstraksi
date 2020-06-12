82373880d8565f5b8a08be8e7c2b146fb6989be8
AfifurRochman <61938343+AfifurRochman@users.noreply.github.com>
2020-06-12 19:25:37 +0700

    Create siomay_Interface.cs

---
+++ siomay_Interface.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface
+{
+    public class siomay : IElektronik;
+    {
+        public void smartphone()
+        {
+            Console.WriteLine("ini adalah barang elektronik jenis smartphone yang memiliki versi");
+            Console.WriteLine("note, note+ , ");
+        }
+    }
+}
