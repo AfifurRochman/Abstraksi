50c261f9b2294d343268554caeb86527a98b5315
AfifurRochman <61938343+AfifurRochman@users.noreply.github.com>
2020-06-12 19:23:04 +0700

    Create Elektronik.cs

---
+++ Elektronik.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstraction
+{
+    public abstract class Elektronik
+    {
+        public abstract void smartphone();
+    }
+}
