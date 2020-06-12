ce733aa34f40f2d40b0fbe3d28746d6f849415d5
AfifurRochman <61938343+AfifurRochman@users.noreply.github.com>
2020-06-12 19:25:13 +0700

    Create pipo_Interface.cs

---
+++ pipo_Interface.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface
+{
+    public class pipo : IElektronik
+    {
+        public void smartphone()
+        {
+
+            Console.WriteLine("Ini adalah barang elektronik jenis pipo yang memiliki versi: ");
+            Console.WriteLine("v,y,");
+        }
+    }
+}
