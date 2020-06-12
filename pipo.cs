6079631ec5a2b956856c6bc0ba3e625d9af0c504
AfifurRochman <61938343+AfifurRochman@users.noreply.github.com>
2020-06-12 19:23:24 +0700

    Create pipo.cs

---
+++ pipo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstraction
+{
+    public class pipo : Elektronik 
+    {
+        public override void smartphone()
+        {
+            Console.WriteLine("Ini adalah barang elektronik jenis pipo yang memiliki versi: ");
+            Console.WriteLine("v,y,");
+
+        }
+    }
+}
